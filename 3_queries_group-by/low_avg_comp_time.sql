465453755850db7eaf0f086d537922972510ab23
colspen <spencerlewiscole@gmail.com>
2022-10-17 20:29:26 +0000

    lint

--- 3_queries_group-by/low_avg_comp_time.sql
+++ 3_queries_group-by/low_avg_comp_time.sql
@@ -1,6 +1,6 @@
 SELECT students.name AS student, 
-ROUND(avg(assignment_submissions.duration), 2) AS average_assignment_duration, 
-ROUND(avg(assignments.duration), 2) AS average_estimated_duration
+ROUND(AVG(assignment_submissions.duration), 2) AS average_assignment_duration, 
+ROUND(AVG(assignments.duration), 2) AS average_estimated_duration
   FROM students
 
 JOIN assignment_submissions ON student_id = students.id
@@ -8,7 +8,8 @@ JOIN assignments ON assignments.id = assignment_id
   WHERE end_date IS NULL
 
 GROUP BY student
-HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+HAVING AVG(assignment_submissions.duration) < 
+AVG(assignments.duration)
   ORDER BY average_assignment_duration;
 
  
