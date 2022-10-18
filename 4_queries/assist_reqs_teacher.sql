732be59907e6efdae601ae1a69893ac8839b81c2
colspen <spencerlewiscole@gmail.com>
2022-10-18 13:38:24 +0000

    student, teacher and assistance request queries

---
+++ 4_queries/assist_reqs_teacher.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(assistance_requests.*) AS total_assistances, name 
+  FROM teachers
+JOIN assistance_requests 
+ON assistance_requests.teacher_id = teachers.id
+
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;
