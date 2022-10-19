f1ced0613ea88886b51bb229de94ebfc37754e9c
colspen <spencerlewiscole@gmail.com>
2022-10-19 21:31:58 +0000

    create teachers.js and write 1st query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -22,37 +22,25 @@ const pool = new Pool({
 //   })
 // });
 
-// pool.query(`
-// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// WHERE cohorts.name LIKE '%${argv[0]}%'
-// LIMIT ${argv[1] || 5};
-// `)
-// .then(res => {
-//   console.log(res.rows)
-//   res.rows.forEach(user => {
-//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-//   })
-// }).catch(err => console.error('query error', err.stack));
-
 pool.query(`
-SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort,
-COUNT(assistance_requests) AS total_assistances
-  FROM teachers
-JOIN assistance_requests 
-  ON teacher_id = teachers.id
-JOIN students
-  ON students.id = student_id
-JOIN cohorts
-  ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${argv[0] || 'JUL02'}'
-GROUP BY teachers.name, cohorts.name
-ORDER BY teacher;
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${argv[0]}%'
+LIMIT ${argv[1] || 5};
 `)
 .then(res => {
+  console.log(res.rows)
   res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
 
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
