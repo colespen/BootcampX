4d53d3f468c6c5abc8488d41085c60cc6c8ab4d4
colspen <spencerlewiscole@gmail.com>
2022-10-19 22:15:23 +0000

    Update students.js and teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,38 +9,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query(`
-// SELECT students.id, students.name AS student, cohorts.name AS cohort
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// // LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res.rows);
-//   res.rows.forEach(user => {
-//     console.log(`${user.student} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-//   })
-// });
-
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${argv[0]}%'
-LIMIT ${argv[1] || 5};
-`)
-.then(res => {
-  console.log(res.rows)
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
-
-
-const queryString = `
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+const queryString =
+  `SELECT students.id as student_id, students.name as name, cohorts.name as cohort
   FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE $1
-  LIMIT $2;
-  `;
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const values = [`%${argv[0]}%`, argv[1]];
+
+pool.query(queryString, values)
+  .then(result => {
+    console.log(result.rows);
+    result.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,17 +9,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString =
+  `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${argv[0] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+`;
+
+const values = [`%${argv[0]}%`];
+pool.query(queryString, values)
+  .then(result => {
+    result.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
   })
-});
+  .catch(err => console.error('query error', err.stack));
